5342abac4f8f9e3acc1f27e700bfda668af6fad9
Dieg0ard <diego.ardilabueno@gmail.com>
2025-02-19 15:10:31 -0500

    añadir la segunda tarea

---
+++ PRIMER CORTE/TAREA_2/FALTA_ENTRE_100/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    public static void main(String[] args) {
+
+        int[] array = new int[99];  
+        Algoritmo algoritmo = new Algoritmo();
+
+        int index = 0;
+        for (int i = 1; i <= 100; i++) {
+            //Se llena el array con números del 1 al 100 menos el de la condición
+            if (i != 15) {
+                array[index] = i;
+                index++;
+            }
+        }
+        //Se imprime el resultado
+        System.out.println("El número faltante es: " + algoritmo.encontrarFaltante(array));
+    }
+
+}
