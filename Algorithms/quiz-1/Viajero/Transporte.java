792b6816308e1b6616c0788d0fc4f0bd770b3b29
Dieg0ard <diego.ardilabueno@gmail.com>
2025-03-01 23:15:10 -0500

    quiz added

---
+++ Algorithms/quiz-1/Viajero/Transporte.java
@@ -0,0 +1,11 @@
+package Viajero;
+
+public class Transporte extends Gastos {
+    public Transporte(){
+        super();
+    }
+
+    public Transporte(int valor){
+        super(valor);
+    }
+}
