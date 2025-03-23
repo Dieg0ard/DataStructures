d10bf45048061153a035a04064b6b98544bbadd7
Dieg0ard <diego.ardilabueno@gmail.com>
2025-03-23 17:06:50 -0500

    merge sort

--- Structures/GenericDoublyLinkedList/DoublyLinkedList.java
+++ Structures/GenericDoublyLinkedList/DoublyLinkedList.java
@@ -1,18 +1,20 @@
 package GenericDoublyLinkedList;
 
-public class DoublyLinkedList<E> {
+public class DoublyLinkedList<E extends Comparable<E>> {
+
+    // Attributes
     private Node<E> first;
     private Node<E> last;
     private int size;
 
+    // Constructor
     public DoublyLinkedList() {
         this.size = 0;
         this.first = null;
         this.last = null;
     }
 
-    // ADD METHODS
-
+    // Add Methods
     public void add(E value) {
         addLast(value);
     }
@@ -67,8 +69,7 @@ public class DoublyLinkedList<E> {
         size++;
     }
 
-    // REMOVE METHODS
-
+    // Remove Methods
     public void removeFirst() {
         if (this.size == 0) {
             throw new RuntimeException("List is empty");
@@ -109,7 +110,6 @@ public class DoublyLinkedList<E> {
             removeLast();
             return;
         }
-
         Node<E> temp = first;
         for (int i = 0; i < index; i++) {
             temp = temp.getNext();
@@ -125,8 +125,7 @@ public class DoublyLinkedList<E> {
         size = 0;
     }
 
-    // GETTERS
-
+    // Getters
     public int getSize() {
         return size;
     }
@@ -135,8 +134,7 @@ public class DoublyLinkedList<E> {
         return size == 0;
     }
 
-    // PRINT METHODS
-
+    // Print Methods
     public void display() {
         Node<E> temp = first;
         while (temp != null) {
@@ -151,5 +149,76 @@ public class DoublyLinkedList<E> {
             System.out.println(" " + temp.getValue() + " ");
             temp = temp.getPrev();
         }
+    }
+
+    // Sorting Methods
+    public void sort() {
+        if (size < 2)
+            return;
+        first = mergeSort(first);
+
+        // Update `last` reference
+        last = first;
+        while (last.getNext() != null) {
+            last = last.getNext();
+        }
+    }
+
+    private Node<E> mergeSort(Node<E> head) {
+        if (head == null || head.getNext() == null)
+            return head;
+
+        Node<E> middle = getMiddle(head);
+        Node<E> secondHalf = middle.getNext();
+        middle.setNext(null);
+        if (secondHalf != null)
+            secondHalf.setPrev(null);
+
+        Node<E> left = mergeSort(head);
+        Node<E> right = mergeSort(secondHalf);
+
+        return merge(left, right);
+    }
+
+    private Node<E> getMiddle(Node<E> head) {
+        if (head == null)
+            return null;
+
+        Node<E> slow = head, fast = head;
+        while (fast.getNext() != null && fast.getNext().getNext() != null) {
+            slow = slow.getNext();
+            fast = fast.getNext().getNext();
+        }
+        return slow;
+    }
+
+    private Node<E> merge(Node<E> left, Node<E> right) {
+        Node<E> dummy = new Node<>(null); // Auxiliary node
+        Node<E> current = dummy;
+
+        while (left != null && right != null) {
+            if (left.getValue().compareTo(right.getValue()) <= 0) {
+                current.setNext(left);
+                left.setPrev(current);
+                left = left.getNext();
+            } else {
+                current.setNext(right);
+                right.setPrev(current);
+                right = right.getNext();
+            }
+            current = current.getNext();
+        }
+
+        if (left != null) {
+            current.setNext(left);
+            left.setPrev(current);
+        }
+        if (right != null) {
+            current.setNext(right);
+            right.setPrev(current);
+        }
+
+        dummy.getNext().setPrev(null);
+        return dummy.getNext();
     }
 }
